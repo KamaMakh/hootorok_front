6fc69cbe25271b8c67e4870461aa4a553e67d599
Arthur Abdrakhmanov <art.abdrakhmanov@yandex.ru>
2019-08-11 18:52:32 +0300

    create page and map (fixes #22)

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-page class="q-pa-md text-center">
+  <q-page padding>
     <h1 class="text-h2" v-text="$t('about')"/>
-    <p class="text-body1 q-pa-sm">
+    <p class="text-body1 q-py-sm">
     Краткое описание базы
     </p>
     <div id="map" style="width: 100%; height: 500px"></div>
-    <p class="text-body1 text-weight-bold q-pa-sm">
+    <p class="text-body1 text-weight-bold q-py-sm text-center">
     Контакты
     </p>
 

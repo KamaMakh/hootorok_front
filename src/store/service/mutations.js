27c8336093b77135b01d53e3e5a87566b2964539
Arthur <49252370+ArthurAbd@users.noreply.github.com>
2019-08-18 14:44:04 +0300

    Update mutations.js

--- src/store/service/mutations.js
+++ src/store/service/mutations.js
@@ -1,17 +1,7 @@
-function setUser(state, user) {
-  state.user = user;
-}
-
-function resetUser(state) {
-  state.user = {};
-}
-
 function updateServices(state, services) {
   state.services = services;
 }
 
 export {
-  setUser,
-  resetUser,
   updateServices,
 };

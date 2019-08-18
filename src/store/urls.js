0e83f4789aa83791eebc76f7d661145daf4b7e6a
Arthur <49252370+ArthurAbd@users.noreply.github.com>
2019-08-18 17:15:46 +0300

    update page (fixes #5)

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,7 +1,6 @@
 const serverUrl = process.env.DEV ? 'http://localhost:13579' : 'https://baza-hutorok.com:13579';
 const registerUrl = `${serverUrl}/auth/register`;
-// const servicesUrl = `${serverUrl}/services/getAllServices`;
-const servicesUrl = 'https://jsonplaceholder.typicode.com/photos?_limit=10';
+const servicesUrl = `${serverUrl}/services/getAllServices`;
 
 export {
   serverUrl,

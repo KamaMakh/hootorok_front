23eabd27e44f225d95503200d69caca05cf5fc54
Arthur <49252370+ArthurAbd@users.noreply.github.com>
2019-08-18 14:50:17 +0300

    update (fixes #5)

--- src/store/service/actions.js
+++ src/store/service/actions.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { servicesUrl } from 'src/store/urls';
 
-
 function getServices({ commit }) {
   return new Promise((resolve, reject) => {
     axios.get(servicesUrl)

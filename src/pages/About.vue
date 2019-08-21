c9dca5377d6566af160f2b80d99801333e59faac
Arthur Abdrakhmanov <art.abdrakhmanov@yandex.ru>
2019-08-22 00:24:07 +0300

    fix (fixes #22)

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -4,7 +4,8 @@
     <p class="text-body1 q-pb-md">
       {{ aboutInfo.description }}
     </p>
-      <yandex-map :coords="coords" :settings="settings">
+      <yandex-map :coords="coords" :settings="settings"
+                  :zoom="zoom" :controls="controls">
         <ymap-marker :coords="coords"></ymap-marker>
       </yandex-map>
     <p class="text-body1 text-weight-bold q-pt-md">
@@ -24,13 +25,24 @@ export default {
   name: 'About',
   components: { yandexMap, ymapMarker },
   data: () => ({
-    coords: [54, 39],
+    zoom: 10,
+    coords: [60, 30],
     settings: {
       apiKey: 'bff47474-50d8-4300-80b7-d342d8cfc443',
       lang: 'ru_RU',
       coordorder: 'latlong',
       version: '2.1',
     },
+    controls: [
+      'fullscreenControl',
+      'geolocationControl',
+      'routeEditor',
+      'rulerControl',
+      'trafficControl',
+      'typeSelector',
+      'zoomControl',
+      'routeButtonControl',
+    ],
   }),
   computed: {
     aboutInfo() {
@@ -47,6 +59,6 @@ export default {
 
 <style scoped>
 .ymap-container {
-  height: 100%;
+  height: 600px;
 }
 </style>

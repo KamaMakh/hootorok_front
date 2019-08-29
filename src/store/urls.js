0caa248bcfda81aecbccc7428709256b158a0366
Alexandra Ivanova <alexkor231@gmail.com>
2019-08-29 22:30:59 +0300

    update store

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,5 +1,9 @@
 const serverUrl = process.env.DEV ? 'http://localhost:13579' : 'https://baza-hutorok.com:13579';
+const getHousingList = `${serverUrl}/rooms/getHousingList`;
+const getMany = `${serverUrl}/rooms/getMany`;
 
 export default {
   serverUrl,
+  getHousingList,
+  getMany,
 };

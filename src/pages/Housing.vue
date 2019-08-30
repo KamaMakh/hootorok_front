afc0f84f59ed8e87351cf70624f93ab4a090bb24
applesFather <otetcyablok@gmail.com>
2019-08-30 14:58:33 +0300

    Style corrections

--- src/pages/Housing.vue
+++ src/pages/Housing.vue
@@ -1,43 +1,55 @@
 <template>
-  <q-page class="housing">
-    <h1 class="housing__title text-center" v-text="$t('housing_facilities')"/>
+  <q-page>
+    <h1 class="text-center text-h4" v-text="$t('housing_facilities')"/>
     <div class="row">
-      <div class="q-mb-md housing__card" v-for="(housing,i) in housings" :key="housing.id">
-        <router-link :to="{ name: 'rooms' }" class="housing__link">
-          Заголовок
-        </router-link>
+      <div
+        v-for="(housing, i) in housings"
+        :key="housing.id"
+        class="q-mb-md full-width text-center q-gutter-y-md"
+      >
+        <router-link
+          :to="{ name: 'rooms' }"
+          class="text-h6 text-default housing-title block"
+          v-text="'Заголовок'"
+        />
         <q-carousel
           v-model="housingsSlides[i]"
-          class="housing__carousel"
+          class="full-width"
           animated
-          navigation
-          height="60vh"
+          thumbnails
+          height="70vh"
           transition-prev="slide-right"
           transition-next="slide-left"
           infinite
           :fullscreen.sync="housingsFullscreens[i]"
         >
           <q-carousel-slide
-            class="housing__slide"
             :name="index"
-            v-for='(img,index) in housing.photos'
+            v-for='(img, index) in housing.photos'
             :key="index"
             :img-src="img"
           />
-          <template v-slot:control>
+          <!-- <template v-slot:control>
             <q-carousel-control
               position="bottom-right"
               :offset="[18, 18]"
             >
               <q-btn
-                push round dense color="white" text-color="primary"
+                push
+                round
+                dense
+                color="white"
+                text-color="primary"
                 :icon="housingsFullscreens[i] ? 'fullscreen_exit' : 'fullscreen'"
                 @click="changeFullscreen(i)"
               />
             </q-carousel-control>
-          </template>
+          </template> -->
         </q-carousel>
-        <p class="housing__description" v-text="housing.description"  />
+        <p
+          class="text-body2 q-px-md housing-description text-left"
+          v-text="housing.description"
+        />
       </div>
     </div>
   </q-page>
@@ -72,28 +84,12 @@ export default {
 };
 </script>
 
-<style scoped lang='stylus'>
-.card-link
-  text-decoration none
-  display block
-  text-align center
-.housing
-  &__title
-    font-size: 28px
-    margin 0
-    margin-bottom 17px
-  &__card
-    width 100%
-  &__link
-    font-size 20px
-    text-decoration none
-    text-align center
-    display block
-    margin-bottom 15px
-    color: $text_default
-  &__carousel
-    width: 100vw
-    margin-bottom 17px
-  &__description
-    font-size: 14px
+<style lang='stylus' scoped>
+.housing-title
+  text-decoration: none
+
+.housing-description
+  max-width: 800px
+  margin-left: auto
+  margin-right: auto
 </style>

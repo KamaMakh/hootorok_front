e008ae4261b8d3529c8868c2eddbe4e972a29cbe
Alexey <aaivanov23@gmail.com>
2019-08-31 00:25:44 +0300

    added one room pages

---
+++ src/pages/RoomItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page padding>
+    <h1 class="text-h5">{{$t('room_number')}} {{this.$attrs.id}}</h1>
+    <div class="q-pa-md">
+      <div class="row">
+        <div class="col">
+          .col
+        </div>
+        <div class="col">
+          <app-booking-card />
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import BookingCard from '../components/BookingCard';
+
+export default {
+  name: 'RoomItem',
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    appBookingCard: BookingCard,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>

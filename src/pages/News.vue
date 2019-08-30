a19b5c6b4be70661e98b86bf5caed0f20fdd98c0
applesFather <otetcyablok@gmail.com>
2019-08-30 16:04:22 +0300

    Style corrections

--- src/pages/News.vue
+++ src/pages/News.vue
@@ -1,16 +1,25 @@
 <template>
-  <q-page padding>
-    <h1 class="text-h4" v-text="$t('news_and_campaigns')"/>
+  <q-page>
+    <h1
+      class="text-h4 q-px-md"
+      v-text="$t('news_and_campaigns')"
+    />
 
-    <div class="q-pa-md justify-end items-center row">
-      <div class="text-grey q-mr-sm">
-        {{ $t('display_by') }}
-      </div>
+    <div class="justify-end items-center row q-px-md">
+      <div
+        class="text-grey q-mr-sm"
+        v-text="$t('display_by')"
+      />
       <div class="q-gutter-md" style="width: 80px; max-width: 100%;">
-        <q-select v-model="perPage" :options="options" outlined dense/>
+        <q-select
+          dense
+          outlined
+          v-model="perPage"
+          :options="perPageSelection"
+        />
       </div>
     </div>
-    <div class="q-pa-md row items-start justify-center q-gutter-md q-mx-auto">
+    <div class="row items-start justify-center q-gutter-md q-mx-auto q-mt-md">
       <q-card
         flat
         bordered
@@ -18,30 +27,46 @@
         :key="item.id"
         class="col-md-4 col-sm-6 col-xs-12 unelevated no-border q-mx-none q-px-md"
       >
-        <q-card-section class="q-py-none q-px-sm q-mb-md">
-          <div class="row items-center no-wrap">
-            <div class="col ellipsis">
-              {{ item.title }}
-            </div>
-          </div>
+        <q-card-section class="q-pa-none q-mb-md text-bold">
+          <router-link
+            :to="{ name: 'news-detail', params: { id: item.id } }"
+            class="row items-center no-wrap standard-link text-default"
+            v-text="item.title"
+          />
         </q-card-section>
 
-        <img class="q-mb-sm q-px-sm" :src="item.main_image">
+        <router-link
+          :to="{ name: 'news-detail', params: { id: item.id } }"
+        >
+          <img
+            :src="item.main_image"
+            class="q-mb-sm"
+            style="max-width: 100%;"
+          >
+        </router-link>
 
         <q-separator class="q-mx-none"/>
 
-        <q-card-section class="q-pa-none q-my-sm q-px-sm">
-          <div class="text-subtitle2">{{ formatDate(item.created_at) }}</div>
+        <q-card-section class="q-pa-none q-my-sm">
+          <div
+            class="text-subtitle2"
+            v-text="formatDate(item.created_at)"
+          />
         </q-card-section>
 
         <div class="q-pa-none q-my-md row flex justify-between items-center">
-          <q-card-section class="q-pa-none q-px-sm ellipsis-2-lines text-grey col-9">
-            {{ item.description }}
-          </q-card-section>
+          <q-card-section
+            class="q-pa-none ellipsis-2-lines text-grey col-9"
+            v-text="item.description"
+          />
 
           <q-card-actions class="col-3 justify-end">
-            <q-btn flat class="justify-center items-center" color="grey">
-              <i class="material-icons text-body2">arrow_forward_ios</i>
+            <q-btn
+              flat
+              color="grey"
+              class="justify-center items-center"
+            >
+              <q-icon name="arrow_forward_ios" style="font-size: 14px;"/>
               <router-link
                 :to="{ name: 'news-detail', params: { id: item.id } }"
                 class="standard-link text-black"
@@ -56,8 +81,7 @@
         v-model="currentPage"
         :max="pages"
         :direction-links="true"
-      >
-      </q-pagination>
+      />
     </div>
   </q-page>
 </template>
@@ -65,14 +89,16 @@
 <script>
 import { mapState } from 'vuex';
 
+const perPageSelection = [6, 12, 18];
+
 export default {
   name: 'News',
   data() {
     return {
       newsList: [],
       droppedList: [],
-      options: [6, 6, 12, 18],
-      perPage: 6,
+      perPageSelection,
+      perPage: perPageSelection[0],
       currentPage: 1,
     };
   },
@@ -117,6 +143,3 @@ export default {
   },
 };
 </script>
-
-<style>
-</style>

27a9bc15999829c5f39820f55cfedcad4e50f47d
applesFather <otetcyablok@gmail.com>
2019-09-01 19:18:41 +0300

    Some styles correction

--- src/pages/info/Info.vue
+++ src/pages/info/Info.vue
@@ -1,19 +1,19 @@
 <template>
   <q-page padding>
     <h2 v-text="$t('reference_information')"/>
-    <div class="row column q-gutter-y-md">
+    <div class="row column q-gutter-y-md items-start">
       <router-link
-        class="standard-link text-default"
+        class="text-default"
         :to="{ name: 'booking-info' }"
         v-text="$t('booking')"
       />
       <router-link
-        class="standard-link text-default"
+        class="text-default"
         :to="{ name: 'payment-info' }"
         v-text="$t('payment')"
       />
       <router-link
-        class="standard-link text-default"
+        class="text-default"
         :to="{ name: 'faq' }"
         v-text="$t('faq')"
       />
